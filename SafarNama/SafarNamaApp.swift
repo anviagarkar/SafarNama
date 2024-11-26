188d954bca85eede53c3127401a9b7e808ab80e6
anviagarkar <139617645+anviagarkar@users.noreply.github.com>
2024-11-26 01:08:13 -0500

    Initial Commit

---
+++ SafarNama/SafarNamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafarNamaApp.swift
+//  SafarNama
+//
+//  Created by Anvi Agarkar on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SafarNamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
